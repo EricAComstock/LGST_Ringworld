00ef39fe42ac9805ac78e5e21f3d417512c40359
thegaug <34422000+thegaug@users.noreply.github.com>
2025-04-22 10:55:57 -0400

    Add files via upload

--- LeakRate.py
+++ LeakRate.py
@@ -8,19 +8,23 @@ Created on Fri Mar 14 12:01:17 2025
 import math as math
 import pandas as pd
 
+###CHANGE FILE###
+
+file = 'particle_data_20250304_123751.xlsx'
+
 ###CONSTANTS####
 
 P_0 = 101325 #Pa
 K_b = 1.380649e-23 #J/K
 T_0 = 300 #K
-m = 2.6566962e-26 #kg
+m = 2.6566962e-26*2 #kg
 g = 9.81 #m/s^2
 n_0 = 2.687e25 #1/m^3
-d = 0.000359 #m  7.3e-11 #m
+d = 3.59e-10 #m  7.3e-11 #m
 
 ###CALCULATE F_ESCAPE###
 
-df = pd.read_excel('particle_data_20250304_123751.xlsx', sheet_name='Particles')
+df = pd.read_excel(file, sheet_name='Particles')
 target_words = ["resimulate", "recaptured", "escaped"]
 counts = [int((df['Result'].astype(str) == word).sum()) for word in target_words]
 
@@ -38,7 +42,8 @@ alt = h_s*math.log(h_s/lam)
 
 n = (P_0/(K_b*T_0))*math.e**(-alt/h_s)
 
-phi_m = n*m
+phi_m = n*m*340*f_escape
 
 print(phi_m)
 
+print('Lifetime', (P_0 / g) / phi_m/86400/365.2425, 'yrs')

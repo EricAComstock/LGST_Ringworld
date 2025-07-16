2882a06c899e433c5430bb8059e6f479bde838cc
JamesChambers-GT <chambers.james.b@gmail.com>
2025-07-15 22:24:47 -0400

    Update StochasticInput.py
    Now includes functionality for charged and varied mass particles. New addition to the inputs of stochastic initial conditions

--- StochasticInput.py
+++ StochasticInput.py
@@ -32,25 +32,35 @@ def SIVarInput(T_i,m_i,y_min_i,y_max_i,z_length_i,y_floor_i):
 
     
 
-def stochastic_initial_conditions(m, T, y_min, y_max, z_length):
+def stochastic_initial_conditions(T, y_min, y_max, z_length, comp_list = None):
     """
     Generates stochastic initial conditions for particle position and velocity.
 
     Parameters:
-    m (float): Particle mass (kg)
     T (float): Temperature (K)
     y_min (int): Minimum y-coordinate for particle spawning (m)
     y_max (int): Maximum y-coordinate for particle spawning (m)
     z_length (int): Total z-length of the domain (m)
+    comp_list (list of tuples (string, float, float, float)): List of all species at a given altitude. 
+        Individual species tuples contain species name, mass, charge, and density (kg, C, particles/m^3)
 
     Returns:
-    list: [x, y, z, vx, vy, vz] initial position and velocity components
+    list: [x, y, z, vx, vy, vz, m, q] initial position and velocity components, and particle mass and charge
     """
     z_min = -int(z_length / 2)
     z_max = int(z_length / 2)
     z     = random.randint(z_min, z_max)   # Generate random z distance from left wall
     y     = random.randint(y_min, y_max)   # Generate random y altitude
     x     = 0                              # Start at x = 0
+
+    total_density = 0
+    for particle in comp_list: total_density += particle[3]
+    weights = []
+    for particle in comp_list: weights.append(particle[3]/total_density)
+    ind = np.random.choice(a=range(len(comp_list)),size=1,p=weights)
+    selected_particle = comp_list[ind[0]]
+    m = selected_particle[1]
+    q = selected_particle[2]
 
     random_vector = np.random.normal(size=3)                       # Generate random 3D vector
     unit_vector   = random_vector / np.linalg.norm(random_vector)  # Normalize to unit length
@@ -61,11 +71,11 @@ def stochastic_initial_conditions(m, T, y_min, y_max, z_length):
 
     [vx, vy, vz] = [float(v) for v in velocity_magnitude * unit_vector]  # Scale unit vector by magnitude
 
-    return [x, y, z, vx, vy, vz]
+    return [x, y, z, vx, vy, vz, m, q]
 
 
 
 # Testing code - only runs when this file is executed directly
 if __name__ == "__main__":
-    input_test = stochastic_initial_conditions(m, T, y_min, y_max, z_length)
+    input_test = stochastic_initial_conditions(T, y_min, y_max, z_length, [("X",1,-1,6), ("Y",2,1,1)])
     print(input_test)
